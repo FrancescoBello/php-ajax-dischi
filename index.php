3c04c787b96c3cabf78e3afe9ed8c324b9c0f25e
Francesco <fraancesco.bello@gmail.com>
2020-12-17 17:02:19 +0100

    Aggiunto Logo

--- index.php
+++ index.php
@@ -9,22 +9,24 @@
   </head>
   <body>
     <header>
-
+      <div class="logo">
+        <img src="dist/img/logo.png" alt="">
+      </div>
     </header>
     <main>
       <div class="container">
           <?php foreach ($dischi as  $disco) {?>
             <div class="album">
-              <img src="<?php $disco["poster"] ?>" alt="">
-              <p> <?php  $disco["title"]?> </p>
-              <p><?php $disco["author"] ?></p>
-              <p><?php $disco["year"] ?></p>
+              <img src="<?php $disco['poster'] ?>" alt="">
+              <p> <?php  $disco['title']?> </p>
+              <p><?php $disco['author'] ?></p>
+              <p><?php $disco['year'] ?></p>
             </div><?php
           }?>
 
 
       </div>
     </main>
-    <script src="main.js" charset="utf-8"></script>
+
   </body>
 </html>
