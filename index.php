df412090aabde074de1882abcce432621d022b73
Francesco <fraancesco.bello@gmail.com>
2020-12-17 16:34:22 +0100

    stampa dischi con php

--- index.php
+++ index.php
@@ -5,8 +5,26 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+    <link rel="stylesheet" href="dist/app.css">
   </head>
   <body>
+    <header>
+
+    </header>
+    <main>
+      <div class="container">
+          <?php foreach ($dischi as  $disco) {?>
+            <div class="album">
+              <img src="<?php $disco["poster"] ?>" alt="">
+              <p> <?php  $disco["title"]?> </p>
+              <p><?php $disco["author"] ?></p>
+              <p><?php $disco["year"] ?></p>
+            </div><?php
+          }?>
+
+
+      </div>
+    </main>
     <script src="main.js" charset="utf-8"></script>
   </body>
 </html>
